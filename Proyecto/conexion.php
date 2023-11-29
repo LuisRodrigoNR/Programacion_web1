1201a3a9085a667c87b754219e919a7f31d76be4
LuisRodrigoNR <luisrnr2910@gmail.com>
2023-11-29 13:22:16 -0600

    cambios en la base

--- Proyecto/conexion.php
+++ Proyecto/conexion.php
@@ -1,12 +1,12 @@
 <?php
-$host_db = "localhost:3308";
+$host_db = "localhost:3306";
 $user_db = "root";
 $pass_db = "1234";
-$db_name = "asistencia";
+$db_name = "vagabundos";
 
 $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
 
 if ($conexion->connect_error) {
- die("La conexion falló jiji: " . $conexion->connect_error);
+ die("La conexion falló : " . $conexion->connect_error);
 }
 ?>
